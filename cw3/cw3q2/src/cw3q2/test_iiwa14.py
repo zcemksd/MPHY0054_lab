709b87a606d5dba4f156a7610d26d362ecf6f536
Your Name <zcemksd@ucl.ac.uk>
2025-01-02 15:58:14 +0000

    done test_iiw14Dyn.py

--- cw3/cw3q2/src/cw3q2/test_iiwa14.py
+++ cw3/cw3q2/src/cw3q2/test_iiwa14.py
@@ -16,7 +16,7 @@ try:
 except Exception as e:
     print("An error occurred during imports:", e)
 
-def compare_matrices(mat1, mat2, tol=1e-6):
+def compare_matrices(mat1, mat2, tol=1e-4):
     print("Comparing matrices...")
     return np.allclose(mat1, mat2, atol=tol)
 
@@ -37,16 +37,28 @@ def main():
         print("Computing B matrices...")
         B_student = student.get_B(joint_readings)
         B_kdl = kdl.get_B(joint_readings)
+        print("B matrix (Student):")
+        print(B_student)
+        print("B matrix (KDL):")
+        print(B_kdl)
         print("B matrices are equal:", compare_matrices(B_student, B_kdl))
 
         print("Cmputing C matrices...")
         C_student = student.get_C_times_qdot(joint_readings, joint_velocities)
         C_kdl = kdl.get_C_times_qdot(joint_readings, joint_velocities)
+        print("C matrix (Student):")
+        print(C_student)
+        print("C matrix (KDL):")
+        print(C_kdl)
         print("C matrices are equal", compare_matrices(C_student, C_kdl))
 
         print("Computing G vectors...")
         G_student = student.get_G(joint_readings)
         G_kdl = kdl.get_G(joint_readings)
+        print("G vector (Student):")
+        print(G_student)
+        print("G vector (KDL):")
+        print(G_kdl)
         print("G vectors are equal", compare_matrices(G_student, G_kdl))
 
     except Exception as e:
